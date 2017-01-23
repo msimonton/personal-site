e481cfe4009ec0eeb533bf765107bd01d1c8cab2
Marc Simonton <marcasimonton@gmail.com>
2017-01-22 18:42:46 -0700

    blah

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/projects', function(req, res, next)  {
+  res.render('projets')
+})
+
+router.get('/portfolio', function(req,res,next) {
+  res.render('portfolio')
+})
+module.exports = router;
